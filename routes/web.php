fb10f9a343cdbf8295657c0863d715c23b72f112
LukaPrcic12345 <bobsagott17@gmail.com>
2022-07-19 19:06:04 +0200

    User can login

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,12 @@ Route::post('/users',[UserController::class, 'store']);
 //Log user out
 Route::post('/logout',[UserController::class,'logout']);
 
+//Show login form
+Route::get('/login', [UserController::class,'login']);
+
+//Log in user
+Route::post('/users/authenticate',[UserController::class,'authenticate']);
+
 
 
 
